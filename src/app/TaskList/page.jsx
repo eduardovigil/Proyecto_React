8b58e2e31b77ba38e0a7a8b05543a204389f6d50
eduardovigil <54220799+eduardovigil@users.noreply.github.com>
2024-10-07 14:13:07 -0600

    Agrego diseño

--- src/app/TaskList/page.jsx
+++ src/app/TaskList/page.jsx
@@ -2,11 +2,13 @@
 import { useState, useContext, useEffect } from "react";
 import axios from "axios";
 import { AuthContext } from "../AuthContext";
+import { useRouter } from "next/navigation";
 
 const TaskList = ({ projectId}) => {
     const [tasks, setTasks] = useState([]);
     const { token } = useContext(AuthContext);
     const [error, setError] = useState(null);
+    const router = useRouter();
 
     useEffect(() => {
         const fetchTasks = async () => {
@@ -23,9 +25,13 @@ const TaskList = ({ projectId}) => {
         };
         fetchTasks();
     }, [projectId,token]);
+    const handleCreateTask = () => {
+        router.push('/tasks');
+    };
     return(
         <div>
         <h2>Tareas del Proyecto</h2>
+        <button onClick={handleCreateTask}>Crear Tarea</button>
         {error && <p>{error}</p>}
         <ul>
             {tasks.length > 0 ? (
